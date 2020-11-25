ed01f86a49d74425f9859899490a1e5a546507f3
malakai <malek.fouhal@gmail.com>
2020-11-25 15:27:29 +0100

    new music, enhanced movements, enhanced respawns, scoring, enhanced start menu

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// score value during the game
+
+var scorecontainer = document.getElementById("score")
+
 //menu theme music
 
 var menuTheme = new Audio();
@@ -7,11 +11,10 @@ menuTheme.src ="/music/menuTheme.mp3";
 menuTheme.play();
 
 //game theme music
+
 var gameTheme = new Audio();
 
 gameTheme.src = "/music/gameTheme.mp3";
-
-//gameTheme.play();
 
 var backgroundPosition = 0;
 
@@ -24,132 +27,6 @@ function startGame() {
     menu.style.display = "none";
     game.style.display = "block";
     gameTheme.play();
-}
-
-// will move the background at a decent rate
-
-function scrollingBackground() {
-    backgroundPosition --;
-    document.getElementById("game").style.backgroundPosition = backgroundPosition + "px 0px";
-    setTimeout(function() {;
-        scrollingBackground();
-}, 10);
-}
-
-scrollingBackground();
-
-//Making the Phantom attack the Hornet!
-
-function scrollingPhantom() {
-    backgroundPosition --;
-    setTimeout(function() {;
-        scrollingPhantom();
-}, 12);
-}
-
-scrollingPhantom();
-
-var phantomPosition = 600;
-
-function movingPhantomLeft() {
-    phantomPosition --;
-    document.getElementById("phantom").style.left = phantomPosition + "px ";
-    console.log("movingPhantomLeft");
-    setTimeout(function() {;
-        randomHeightPhantom();
-        movingPhantomLeft();
-}, 5990);
-}
-
-movingPhantomLeft();
-
-function randomHeightPhantom() {
-    let phantom = document.getElementById("phantom");
-    let height = Math.floor( Math.random() * 425 );
-    phantom.style.bottom = height +"px"
-}
-
-//Making the Banshee attack the Hornet!
-
-function scrollingBanshee() {
-    backgroundPosition --;
-    setTimeout(function() {;
-        scrollingBanshee();
-}, 12);
-}
-
-scrollingBanshee();
-
-var bansheePosition = 600;
-
-function movingBansheeLeft() {
-    bansheePosition --;
-    document.getElementById("banshee").style.left = bansheePosition + "px ";
-    console.log("movingBansheeLeft");
-    setTimeout(function() {;
-        movingBansheeLeft();
-        randomHeightBanshee();
-}, 4990);
-}
-
-movingBansheeLeft();
-
-function randomHeightBanshee() {
-    let banshee = document.getElementById("banshee");
-    let height = Math.floor( Math.random() * 650 );
-    if (height<=450){
-        banshee.style.bottom = "450px"
-    } else {
-        banshee.style.bottom = height +"px";
-    }
-    let banshee2 = document.getElementById("banshee2");
-    let height2 = Math.floor( Math.random() * 425 );
-    if (height2<=350){
-        banshee2.style.bottom = "350px"
-    } else {
-        banshee2.style.bottom = height2 +"px";
-    }
-    let banshee3 = document.getElementById("banshee3");
-    let height3 = Math.floor( Math.random() * 325 );
-        banshee3.style.bottom = height3 +"px";
-}
-
-//Making the Elite attack the Hornet!
-
-function scrollingElite() {
-    backgroundPosition --;
-    setTimeout(function() {;
-        scrollingElite();
-}, 12);
-}
-
-scrollingElite();
-
-var elitePosition = 600;
-
-function movingEliteLeft() {
-    elitePosition --;
-    document.getElementById("elite").style.left = elitePosition + "px ";
-    console.log("movingEliteLeft");
-    setTimeout(function() {;
-        randomHeightElite()
-        movingEliteLeft();
-}, 2990);
-}
-
-movingEliteLeft();
-
-function randomHeightElite() {
-    let elite = document.getElementById("elite");
-    let height = Math.floor( Math.random() * 650 );
-    if (height<=350){
-        elite.style.bottom = "350px"
-    } else {
-        elite.style.bottom = height +"px";
-    }
-    let elite2 = document.getElementById("elite2");
-    let height2 = Math.floor( Math.random() * 310 );
-    elite2.style.bottom = height2 +"px";
 }
 
 //will offer the possibility to go up!
@@ -183,4 +60,116 @@ document.body.onkeydown = function(e){
         movingHornetUp();
     }
 };
+
+// will move the background at a decent rate
+
+function scrollingBackground() {
+    backgroundPosition --;
+    document.getElementById("game").style.backgroundPosition = backgroundPosition + "px 0px";
+    setTimeout(function() {;
+        scrollingBackground();
+}, 2);
+}
+
+scrollingBackground();
+
+//Making the Phantom attack the Hornet!
+
+
+var phantomPosition = 600;
+
+function movingPhantomLeft() {
+    phantomPosition --;
+    document.getElementById("phantom").style.left = phantomPosition + "px ";
+    console.log("movingPhantomLeft");
+}
+
+movingPhantomLeft();
+
+function randomHeightPhantom() {
+    let phantom = document.getElementById("phantom");
+    let height = Math.floor( Math.random() * 425 );
+    phantom.style.bottom = height +"px"
+}
+
+//Making the Banshee attack the Hornet!
+
+
+var bansheePosition = 600;
+
+function movingBansheeLeft() {
+    bansheePosition --;
+    document.getElementById("banshee").style.left = bansheePosition + "px ";
+    console.log("movingBansheeLeft");
+}
+
+movingBansheeLeft();
+
+function randomHeightBanshee() {
+    let banshee = document.getElementById("banshee");
+    let height = Math.floor( Math.random() * 650 );
+    if (height<=450){
+        banshee.style.bottom = "450px"
+    } else {
+        banshee.style.bottom = height +"px";
+    }
+    let banshee2 = document.getElementById("banshee2");
+    let height2 = Math.floor( Math.random() * 425 );
+    if (height2<=350){
+        banshee2.style.bottom = "350px"
+    } else {
+        banshee2.style.bottom = height2 +"px";
+    }
+    let banshee3 = document.getElementById("banshee3");
+    let height3 = Math.floor( Math.random() * 325 );
+        banshee3.style.bottom = height3 +"px";
+}
+
+// Covenants score count
+var score = 0;
+// Covies
+var banshee = document.getElementsByClassName("banshee");
+for (var i = 0; i < banshee.length; i++) {
+    // When a covie gets out of the screen
+    banshee[i].addEventListener("animationiteration", function(){
+        score++;
+        scorecontainer.innerHTML = score;
+        console.log(score);
+        var random = (Math.random()*600);
+        this.style.top = random + "px";
+        this.style.animationPlayState = "paused";
+        var self = this;
+        setTimeout(function() {;
+            self.style.animationPlayState = "running";
+        }, Math.random()*2000);
+    });
+}
+
+
+//Making the Elite attack the Hornet!
+
+
+var elitePosition = 600;
+
+function movingEliteLeft() {
+    elitePosition --;
+    document.getElementById("elite").style.left = elitePosition + "px ";
+    console.log("movingEliteLeft");
+}
+
+movingEliteLeft();
+
+function randomHeightElite() {
+    let elite = document.getElementById("elite");
+    let height = Math.floor( Math.random() * 625 );
+    if (height<=350){
+        elite.style.bottom = "350px"
+    } else {
+        elite.style.bottom = height +"px";
+    }
+    let elite2 = document.getElementById("elite2");
+    let height2 = Math.floor( Math.random() * 310 );
+    elite2.style.bottom = height2 +"px";
+}
+
 
